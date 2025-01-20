02fd7a3b4afeb3cc6592542cabfc92dd541926f0
9halfcirno <baka9cirno@outlook.com>
2025-01-21 03:49:48 +0800

    更新 loadPage.js

--- loadPage.js
+++ loadPage.js
@@ -1,7 +1,7 @@
 console.log("js running");
 window.onload = function() {
     async function getPage(page = "home", type = "html") {
-let target = document.getElementById("pageindex");
+        let target = document.getElementById("pageindex");
         //获取文件
         let file = function() {
             switch (type) {
@@ -23,16 +23,35 @@ let target = document.getElementById("pageindex");
                     break;
             }
         };
+        function setFile(type = "html", tar = "pageindex",file, url) {
+        tar = document.getElementById(tar);
+            switch (type) {
+                case "html":
+                    tar.innerHTML = file;
+                    return "success";
+                    break;
+                case "css":
+                    return '<link id="pagestyles" href=' + url + '/>';
+                    break;
+                case "js":
+                    return '<script id="pagejs" src=' + url + '</script>;
+                    break;
+                default:
+                    console.error("no type")
+                    return "fail";
+                    break;
+            }
+        };
         target.innerHTML = '<h3>LOADING……</h3>';
         let url = "https://9halfcirno.github.io/pages/" + page + "/" + file();
         try {
             let response = await fetch(url);
             if (response) {
                 let html = await response.text();
-                target.innerHTML = html;
+                setFile(type, target.id, html, url)
             } else {
                 console.log(`fail to get page : ${url}`);
-                setTimeout(() => getPage(page,type), 500);
+                setTimeout(() => getPage(page, type), 500);
                 target.innerHTML = '<h1>FAIL:try again after 5s……</h1>';
             }
         } catch (e) {
