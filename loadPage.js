8fa850e640e7a542df89f90b29d411527685507c
9halfcirno <baka9cirno@outlook.com>
2025-01-20 19:36:13 +0800

    更新 loadPage.js

--- loadPage.js
+++ loadPage.js
@@ -1,19 +1,46 @@
 console.log("js running");
 window.onload = function() {
-    async function getPage(url, iframe = false) {
-        let content = document.getElementById("content");
-        content.innerHTML = '<h1>LOADING……</h1>';
-        url = url || "https://9halfcirno.github.io/pages/chat.html";
+    async function getPage(page = "home", type = "html") {
+        //获取文件
+        let file = function() {
+            switch (type) {
+                case "html":
+                    let target = document.getElementById("pageindex");
+                    return "index.html";
+                    break;
+                case "css":
+                    let target = document.getElementById("pagestyles");
+                    return "styles.css";
+                    break;
+                case "js":
+                    let target = document.getElementById("pagejs");
+                    return "script.js";
+                    break;
+                default:
+                    let target = document.getElementById("pageindex");
+                    return "index.html";
+                    break;
+            }
+        };
+        target.innerHTML = '<h3>LOADING……</h3>';
+        let url = "https://9halfcirno.github.io/pages/" + page + "/" + file;
         try {
             let response = await fetch(url);
             if (response) {
                 let html = await response.text();
-                content.innerHTML = html;
-                console.log(`load succeed!`)
+                target.innerHTML = html;
             } else {
                 console.log(`fail to get page : ${url}`);
-                setTimeout(() => getPage(url), 500);
-                content.innerHTML = '<h1>FAIL:try again after 5s……</h1>';
+                setTimeout(() => getPage(page,type), 500);
+                target.innerHTML = '<h1>FAIL:try again after 5s……</h1>';
+            }
+        } catch (e) {
+            console.log(`Error to get html in ${url} :`, e);
+            target.innerHTML = '<h1>ERROR</h1>';
+        }
+    }
+    getPage("chat", "html");
+}; = '<h1>FAIL:try again after 5s……</h1>';
             }
         } catch (e) {
             console.log(`Error to get html in ${url} :`, e);
